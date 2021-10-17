0528d543f41daeb69fa5af8c3e562c8bf8947958
M8OE <91699776+M8OE@users.noreply.github.com>
2021-10-17 18:59:08 -0500

    ye

--- Spirits/check.js
+++ Spirits/check.js
@@ -7,7 +7,7 @@ function checkCode(codeId){
  
     if(answer == attempt) {
         document.getElementById("theShit" + codeId.substring(4)).innerHTML = "<h1>We have been appeased. Go show those the badge to get in</h1><br>"
-        image.url('IMG_0018.JPG');
+        document.body.style.backgroundImage = "url('Ghost.jpg')"
     }
     else{
         document.getElementById("response" + codeId.substring(4)).innerHTML = "<h2>WRONG</h2><br>"
