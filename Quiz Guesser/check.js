8d79e962733194985ce20b5e305a224cdae6b108
M8OE <91699776+M8OE@users.noreply.github.com>
2022-10-11 14:19:27 -0500

    asf

---
+++ Quiz Guesser/check.js
@@ -0,0 +1,14 @@
+function checkCode(codeId) {
+    attempt = document.getElementById(codeId).value
+
+    if (codeId == "code2") {
+        answer = "Excited"
+    }
+
+    if (answer == attempt) {
+        document.getElementById("response" + codeId.substring(4)).innerHTML = "<h4> Correct! </h4><br>"
+    }
+    else {
+        document.getElementById("response" + codeId.substring(4)).innerHTML = "<h2>WRONG</h2><br>"
+    }
+}
